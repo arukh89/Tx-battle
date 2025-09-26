4400c8822a506e669e53bb8d62982f737b5757d4
arukh89 <85455057+arukh89@users.noreply.github.com>
2025-09-26 18:00:04 +0700

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // penting biar path static benar di Netlify
   build: {
     outDir: 'dist',
-    sourcemap: true
-  }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://eb0cfc01-fe64-4864-b040-a73e372b5e93-00-1lhwoxp54udnj.kirk.replit.dev',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
